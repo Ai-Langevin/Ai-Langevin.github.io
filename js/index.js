79e696780a925b1b3c518cab7cc744178d4ac88c
shuaizhu1 <shuai168zhu@gmail.com>
2019-10-14 23:42:59 -0700

    reiko page has modal

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-console.log('ready');
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("reiko");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+// Get the image and insert it inside the modal
+
+document.getElementById("reiko").onclick = function(){
+  console.log('dad')
   modal.style.display = "block";
-  modalImg.src = this.src;
+  document.getElementById("img01").src = this.src;
 }
 
+document.getElementById("reiko1").onclick = function(){
+  modal.style.display = "block";
+  document.getElementById("img01").src = document.getElementById("reikoimg1").src;
+}
+
+document.getElementById("reiko2").onclick = function(){
+  modal.style.display = "block";
+  document.getElementById("img01").src = document.getElementById("reikoimg2").src;
+}
+
+document.getElementById("reiko3").onclick = function(){
+  modal.style.display = "block";
+  document.getElementById("img01").src = document.getElementById("reikoimg3").src;
+}
+
+// close  function
 document.getElementById("myModal").onclick = function() {
   modal.style.display = "none";
 }
