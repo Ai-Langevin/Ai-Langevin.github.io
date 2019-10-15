8ddd8a65166dcfd500b88f78dc30daa45daf47eb
shuaizhu1 <shuai168zhu@gmail.com>
2019-10-14 23:11:31 -0700

    image modal working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 console.log('ready');
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("reiko");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+document.getElementById("myModal").onclick = function() {
+  modal.style.display = "none";
+}
